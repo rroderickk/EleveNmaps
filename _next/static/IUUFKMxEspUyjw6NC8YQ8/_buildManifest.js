9664a39c5faed989b85012676d02e706357c8f32
RodrigoMoreno <rroderickk@gmail.com>
2022-05-09 10:06:58 -0500

    test1

---
+++ _next/static/IUUFKMxEspUyjw6NC8YQ8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/899-359801454dfd17d4.js","static/chunks/pages/index-b8685d36ac027058.js"],"/_error":["static/chunks/pages/_error-6e6353f79838677c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
